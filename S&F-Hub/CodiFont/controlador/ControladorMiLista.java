d4941039e3976c79673198915f5633a1ce1ef0a7
Diego5955 <72852377+Diego5955@users.noreply.github.com>
2021-06-01 13:34:46 +0200

    Add files via upload

--- S&F-Hub/CodiFont/controlador/ControladorMiLista.java
+++ S&F-Hub/CodiFont/controlador/ControladorMiLista.java
@@ -1,6 +1,8 @@
 package controlador;
 
+
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -9,19 +11,32 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class ControladorMiLista implements Initializable{
+public class ControladorMiLista implements Initializable {
 
 	@FXML
 	private Button btnVolver2;
-
+	@FXML
+	private Button NLista;
+	@FXML
+	private Button Añadir;
+	
+	
+	@FXML private Label lblNombreLista;
+	
+	 ControladorPyS stage1_controller_enstage2;
+	
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 		
 		btnVolver2.setOnMouseClicked((event) -> Atras2());
+		NLista.setOnMouseClicked((event) -> NuevaLista());
+		//Añadir.setOnMouseClicked((event) -> AñadirSyP());
 		
 	}
 	
@@ -44,6 +59,27 @@ public class ControladorMiLista implements Initializable{
 		}
 	}
 	
+	public void reciberparametros (ControladorPyS stage1, String texto ) {
+		
+		Añadir.setText(texto);
+		stage1_controller_enstage2=stage1;
+		
+	}
+	
+	private void NuevaLista() {
+		
+		TextInputDialog tid = new TextInputDialog();
+		tid.setHeaderText(null);
+		tid.setTitle("Insertar");
+		tid.setContentText("Nombre de la Lista");
+		Optional<String> texto = tid.showAndWait();
+		
+		String NombreNuevaLista = texto.get();
+		
+		lblNombreLista.setText(NombreNuevaLista);
+		
+	}
+
 }
 	
 
