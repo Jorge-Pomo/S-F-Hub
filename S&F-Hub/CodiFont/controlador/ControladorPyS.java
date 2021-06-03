401a3809663b3cda8c05ffbccd38611a04959770
Jorge-Pomo <50466842+Jorge-Pomo@users.noreply.github.com>
2021-06-03 09:25:05 +0200

    AJUSTES

--- S&F-Hub/CodiFont/controlador/ControladorPyS.java
+++ S&F-Hub/CodiFont/controlador/ControladorPyS.java
@@ -18,29 +18,23 @@ import javafx.stage.Stage;
 
 public class ControladorPyS implements Initializable {
 
-	
-	@FXML
-	private Button btnVolver;
-	@FXML
-	private Button AñadirALista;
-	@FXML
-	private Label Titulo;
-	
-	
-	
+	@FXML private Button btnVolver;
+	@FXML private Button AñadirALista;
+	@FXML private Label Titulo;
+
 	ControladorPyS ControladorPyS;
-	
+
 	@FXML
-	public void recibirparametros ( String texto) {
-	Titulo.setText(Titulo.getText());	
-		
+	public void recibirparametros(String texto) {
+		Titulo.setText(Titulo.getText());
+
 	}
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+
 		btnVolver.setOnMouseClicked((event) -> Atras());
-		ControladorPyS=this;
+		ControladorPyS = this;
 		AñadirALista.setOnMouseClicked((event) -> {
 			try {
 				Añadir();
@@ -49,58 +43,50 @@ public class ControladorPyS implements Initializable {
 				e.printStackTrace();
 			}
 		});
-		
+
 	}
-	
+
 	public void Atras() {
-		
+
 		try {
-		
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/Inicio2.fxml"));
 
-		Parent root = loader.load();
-		Stage stage = (Stage) this.btnVolver.getScene().getWindow();
-		
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/Inicio2.fxml"));
 
-		stage.setTitle("S&F Hub -- Serie");
-		stage.setScene(new Scene(root));
-		stage.show();
+			Parent root = loader.load();
+			Stage stage = (Stage) this.btnVolver.getScene().getWindow();
 
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-	}
-	
-	
-	
-	private void Añadir() throws IOException {
-		
-		try {
-		
-		Stage stage2 = new Stage();
-		
-		FXMLLoader loader = new FXMLLoader();
-		
-		AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MiLista.fxml"));
-		
-		ControladorMiLista ControladorMiListaInstancia=(ControladorMiLista)loader.getController();
+			stage.setTitle("S&F Hub -- Serie");
+			stage.setScene(new Scene(root));
+			stage.show();
 
-		ControladorMiListaInstancia.reciberparametros(ControladorPyS, Titulo.getText()); 
-		
-		
-		Scene scene = new Scene(root);
-		stage2.setScene(scene);
-		stage2.alwaysOnTopProperty();
-		stage2.initModality(Modality.APPLICATION_MODAL);
-		stage2.show();
-		} catch(IOException e) {
-			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	private void Añadir() throws IOException {
+
+		try {
+
+			Stage stage2 = new Stage();
+
+			FXMLLoader loader = new FXMLLoader();
+
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("MiLista.fxml"));
+
+			ControladorMiLista ControladorMiListaInstancia = (ControladorMiLista) loader.getController();
+
+			ControladorMiListaInstancia.reciberparametros(ControladorPyS, Titulo.getText());
+
+			Scene scene = new Scene(root);
+			stage2.setScene(scene);
+			stage2.alwaysOnTopProperty();
+			stage2.initModality(Modality.APPLICATION_MODAL);
+			stage2.show();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
 }
-
-
-	
-
