a559b43ec56da0ae6d41debf50778a078ebece73
Jorge-Pomo <50466842+Jorge-Pomo@users.noreply.github.com>
2021-06-03 10:52:49 +0200

    Add files via upload

---
+++ S&F-Hub/CodiFont/controlador/serie.java
@@ -0,0 +1,32 @@
+package controlador;
+
+public class serie {
+
+	private String titulo = null;
+    private String link = null;
+
+    public serie() {
+    }
+
+    public serie(String firstName, String lastName) {
+        this.titulo = firstName;
+        this.link = lastName;
+    }
+
+    public String getFirstName() {
+        return titulo;
+    }
+
+    public void setFirstName(String firstName) {
+        this.titulo = firstName;
+    }
+
+    public String getLastName() {
+        return link;
+    }
+
+    public void setLastName(String lastName) {
+        this.link = lastName;
+    }
+	
+}
