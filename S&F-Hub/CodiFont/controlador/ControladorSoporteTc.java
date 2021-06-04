579ed93efcf2b24e6a2ed8e1c0762ff551161c85
Diego5955 <72852377+Diego5955@users.noreply.github.com>
2021-06-04 11:16:05 +0200

    Add files via upload

--- S&F-Hub/CodiFont/controlador/ControladorSoporteTc.java
+++ S&F-Hub/CodiFont/controlador/ControladorSoporteTc.java
@@ -1,7 +1,13 @@
 package controlador;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+
+
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,19 +15,32 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import modelo.Usuario;
 
 public class ControladorSoporteTc implements Initializable{
-
+	
 	@FXML
 	private Button btnVolver3;
-
+	@FXML
+	private Button subirError;
+	@FXML
+	private TextArea Texto;
+	@FXML
+	private TextField Email;
+	public Usuario user; 
+	
+	
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 		
 		btnVolver3.setOnMouseClicked((event) -> Atras3());
+		subirError.setOnMouseClicked((event) -> Error());
+		
 		
 	}
 	
@@ -43,5 +62,31 @@ public class ControladorSoporteTc implements Initializable{
 			e.printStackTrace();
 		}
 	}
+	
+	public void Error() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://54.235.194.103/bd_s&fhub", "Conectar",
+					"12345678");
+
+			Statement s = conexion.createStatement();
+			
+			int admin = (int) Math.random()*2+1;
+								
+			ResultSet rs2 = s.executeQuery("SELECT id_usuario FROM usuario where nombre = '" + ControladorLoggin.nombreUsu+"';");
+			
+			rs2.next();
+			
+			int idUsu = rs2.getInt(1);
+			
+			int rs = s.executeUpdate( "INSERT INTO `error`(`Email`, `descripcion`, `id_basico`, `id_admin`) VALUES ('" + Email.getText() + "','" + Texto.getText() + "'," + idUsu + ",'" + admin + "' )");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 	
 }
